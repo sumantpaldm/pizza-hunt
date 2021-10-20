1fac7e3317ae7e6e8bdd943637460b2b59955375
SINGH 1258 <sumantpaldm@gmail.com>
2021-10-20 10:26:02 -0400

    added mongoose curd funtionality

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,68 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+
+    getAllPizza(req, res) {
+        Pizza.find({})
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    // get one pizza by id
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+            .then(dbPizzaData => {
+                //if no pizza found send 404
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+
+    // createPizza (post)
+    createPizza({ body }, res) {
+        Pizza.create(body)
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.status(400).json(err));
+    },
+
+
+    // update pizza by id (put)
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // delete pizza (delete)
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+
+};
+
+module.exports = pizzaController;
---
+++ models/Pizza.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+
+    pizzaName: {
+        type: String
+    },
+
+    createdBy: {
+        type: String
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+
+    size: {
+        type: String,
+        default: 'Large'
+    },
+
+    toppings: [] // or toppings: Array
+
+});
+
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
